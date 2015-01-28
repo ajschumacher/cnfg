376d6f1e47891ceb5f95bcf8b18cae83808dd72b
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-28 17:35:29 +0000

    implement

--- cnfg/cnfg.py
+++ cnfg/cnfg.py
@@ -1,2 +1,24 @@
+import os
+import codecs
+
+
+def contents_of(path):
+    """
+    Return the file contents from `path` using UTF-8.
+    """
+    with codecs.open(path, "r", "utf-8") as f:
+        contents = f.read()
+    return contents
+
 def load(filename):
-    return {}
+    """
+    Load `filename` from the user's home directory
+    and return it `eval`'ed.
+    A Python dict literal containing configuration settings
+    is a good choice for the contents of the file.
+    """
+    implied_path = os.path.join("~", filename)
+    full_path = os.path.expanduser(implied_path)
+    contents = contents_of(full_path)
+    python_thing = eval(contents)
+    return python_thing
